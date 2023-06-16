af149dbefc4fa5348ee9a1c9fffee4907a0bf213
Thien Tran <contact@tommytran.io>
2023-06-16 05:15:09 -0700

    Disable goproxy
    Signed-off-by: Thien Tran <contact@tommytran.io>

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CGO_ENABLED=1 go install -tags extended github.com/gohugoio/hugo@latest
+export GOPROXY=direct
+export GOSUMD=off
+export CGO_ENABLED=1
+
+go install -tags extended github.com/gohugoio/hugo@latest
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   hugo --minify
