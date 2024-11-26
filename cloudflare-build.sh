e64ee7444a815aff1edbae5b8d66fb75c20f1311
Tommy <contact@tommytran.io>
2024-11-26 05:19:55 -0700

    Unpin hugo version

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-#curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*extended.*linux-amd64.tar.gz" | cut -d : -f 2,3 | sed 's/"//g' | xargs wget
+curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*extended.*linux-amd64.tar.gz" | cut -d : -f 2,3 | sed 's/"//g' | xargs wget
 
-wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_0.119.0_Linux-64bit.tar.gz
+# wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_0.119.0_Linux-64bit.tar.gz
 
 tar xvf ./*.tar.gz
 chmod u+x ./hugo
